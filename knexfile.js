354245a819cddd650bd5e75975ec7e7e4bbc5eb4
jphaggerty <james.haggerty.1@spaceforce.mil>
2022-02-07 16:12:06 -0500

    Updated .env file

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,47 @@
-module.exports = {
+require("dotenv").config();
 
+module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgress://postgres:bacon@localhost:5432/postgres'
+    client: "pg",
+    connection: {
+      host : '127.0.0.1',
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      charset: 'utf8',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
